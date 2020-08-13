222d6f35f443dd33fcc833cc7377103394a2c230
dshinzie <dshin@rivt.com>
2020-08-12 22:17:34 -0600

    basic bubble sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public void performSort(int arr[]) {
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/Sorts.java
+++ src/Sorts.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Sorts {
+    public static void main(String args[]) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Array pre-sorted:");
+        System.out.println(Arrays.toString(arr));
+
+        // Bubble sort
+        System.out.println("Bubble sort:");
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.performSort(arr);
+
+        // Insertion sort
+        System.out.println("Insertion sort:");
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.performSort(arr);
+    }
 }
